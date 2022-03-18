f45524886b876b3a655abc8377de0c000f9f168d
alpacarlioglu <alpacarlioglu@gmail.com>
2022-03-18 14:36:13 +0300

    2

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+     
+    NavMeshAgent navMeshAgent;
+    float  variable;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
