290acbd12cbe4faa871238f0c0be27f3fcc1cf64
alpacarlioglu <alpacarlioglu@gmail.com>
2022-03-18 17:58:43 +0300

    EnemyAI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,7 +9,8 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float chaseRange = 5f;
      
     NavMeshAgent navMeshAgent;
-    float  variable;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start()
     {
@@ -17,7 +18,45 @@ public class EnemyAI : MonoBehaviour
     }
 
     void Update()
+    {  
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+
+        else if(distanceToTarget < chaseRange)
+        {   
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    } 
+
+    private void ChaseTarget() 
     {
         navMeshAgent.SetDestination(target.position);
+    }
+        
+    void AttackTarget()
+    {
+        Debug.Log("Attacked");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
