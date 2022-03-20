52b72c84888671e97bae97c1339662168a47fa89
alpacarlioglu <alpacarlioglu@gmail.com>
2022-03-20 11:36:46 +0300

    Ammo2

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{ 
+    [SerializeField] private int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+            Destroy(gameObject);
+        }
+    }
+}
